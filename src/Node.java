3f4e013695fe06e673063b3c1e1b0ad8f75ee98d
hazemallam <48776306+hazemallam@users.noreply.github.com>
2019-06-19 16:46:45 +0200

    Add files via upload

---
+++ src/Node.java
@@ -0,0 +1,13 @@
+
+
+
+
+public class Node {
+    public char label;
+    public boolean wasVisited;
+    
+    public Node(char vertex){
+        label= vertex;
+        wasVisited = false;
+    }
+}
